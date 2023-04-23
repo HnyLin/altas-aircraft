5f20322c9e39e90a6ac2c10d6f9cdd7ad3fb8e9d
HnyLin <hnylin@ucdavis.edu>
2023-04-23 16:37:52 -0700

    Added Requirements

---
+++ source/q2/Refined_Drag_Polars.py
@@ -0,0 +1,14 @@
+'''
+Account for:
+Zero Lift Drag
+Drag due to Flaps
+Lift Induced Drag
+Trim Drag
+
+Required Drag Curves
+Clean
+Takeoff Flaps, Gear Up
+Takeoff Flaps, Gear Down
+Landing Flaps, Gear Up
+Landing Flaps, gear Down
+'''
